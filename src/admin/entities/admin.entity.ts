c6a5c7cb76c5991bcb8dcb27575a255589fb963b
Aleksandr.Popov <aa.popov@i-teco.ru>
2023-07-15 00:26:45 +0300

    add admin entity

--- src/admin/entities/admin.entity.ts
+++ src/admin/entities/admin.entity.ts
@@ -1,1 +1,31 @@
-export class Admin {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { AdminType } from "./admin-type.enum";
+import { AdminDormitory } from "./admin-dorm.enum";
+
+@Entity('admins')
+export class Admin {
+    @PrimaryGeneratedColumn({name: 'admin_id'})
+    adminId: number;
+
+    @Column({unique: true})
+    login: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    fullname: string;
+
+    @Column({
+        type: 'enum',
+        enum: AdminType,
+        name: 'admin_type'
+    })
+    adminType: AdminType
+
+    @Column({
+        type: 'enum',
+        enum: AdminDormitory,
+    })
+    dormitory: AdminDormitory
+}
