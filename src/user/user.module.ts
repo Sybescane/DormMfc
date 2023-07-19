57e536a725728473c84daa4eb26f7cfa826b31e9
Aleksandr.Popov <aa.popov@i-teco.ru>
2023-07-19 14:22:49 +0300

    удалил контроллер user

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
-import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Dormitory } from 'src/dormitory/entity/dormitory.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Dormitory])],
-  controllers: [UserController],
   providers: [UserService],
   exports: [UserService]
 })
