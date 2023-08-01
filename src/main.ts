3611afe824d949d3e937df9cb93e72372f219d7d
Aleksandr.Popov <aa.popov@i-teco.ru>
2023-08-01 22:03:40 +0300

    добавил cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors()
   const config = new DocumentBuilder()
     .setTitle('Education API')
     .setVersion('1.0')
