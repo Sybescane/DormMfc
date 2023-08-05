44945fde14091aa5d01166eed5d6b35a8219125c
Aleksandr.Popov <aa.popov@i-teco.ru>
2023-08-05 20:15:26 +0300

    merge from alexandr

--- backend/dist/src/dormitory/dto/create-dorm.dto.d.ts
+++ backend/dist/src/dormitory/dto/create-dorm.dto.d.ts
@@ -1,5 +1,5 @@
 import { Dormitory } from "src/dormitory/entity/dormitory.entity";
-declare const CreateDormDto_base: import("@nestjs/common").Type<Pick<Dormitory, "name" | "description" | "address">>;
+declare const CreateDormDto_base: import("@nestjs/common").Type<Pick<Dormitory, "description" | "address" | "name">>;
 export declare class CreateDormDto extends CreateDormDto_base {
 }
 export {};
