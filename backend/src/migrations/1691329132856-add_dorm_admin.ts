574f936ff0d59ab3b1933d7400d1cc7b50ed08ce
Aleksandr.Popov <aa.popov@i-teco.ru>
2023-08-06 16:40:40 +0300

    fix admin moks

--- backend/src/migrations/1691329132856-add_dorm_admin.ts
+++ backend/src/migrations/1691329132856-add_dorm_admin.ts
@@ -1,7 +1,7 @@
 import { hash } from "argon2";
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class MokesInAdmin1690715720800 implements MigrationInterface {
+export class AddDormAdmin1691329132856 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         const passwordAlex = await hash('07092003')
