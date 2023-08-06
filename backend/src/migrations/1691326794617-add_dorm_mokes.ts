70a8bb059dcd51639b141c6e2f90b4fade736bde
Aleksandr.Popov <aa.popov@i-teco.ru>
2023-08-06 16:01:36 +0300

    fix dorm mokes

--- backend/src/migrations/1691326794617-add_dorm_mokes.ts
+++ backend/src/migrations/1691326794617-add_dorm_mokes.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class MokesInDorm1689618779763 implements MigrationInterface {
+export class AddDormMokes1691326794617 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
